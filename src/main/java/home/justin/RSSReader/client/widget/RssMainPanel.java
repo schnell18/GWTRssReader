1e3ae605aa1c8a6f62cc4626fc286a63f43c9060
Justin Zhang <schnell18@gmail.com>
2014-08-21 15:22:17 +0800

    Add RSS reader prototype

---
+++ src/main/java/home/justin/RSSReader/client/widget/RssMainPanel.java
@@ -0,0 +1,10 @@
+package home.justin.RSSReader.client.widget;
+
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+
+public class RssMainPanel extends ContentPanel {
+
+    public RssMainPanel() {
+        setHeadingText("Main");
+    }
+}
